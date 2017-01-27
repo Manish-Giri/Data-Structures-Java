b7ee10cd0116420fff7d64092d7a390a16461361
MANISH-GIRI <manish.giri.me@gmail.com>
2017-01-26 23:29:12 -0500

    Stack: Member variables and constructor

--- src/stacksqueues/Stack.java
+++ src/stacksqueues/Stack.java
@@ -4,5 +4,18 @@ package stacksqueues;
  * Created by manishgiri on 1/26/17.
  */
 public class Stack {
-    
+
+    //private members
+    private int maxSize;
+    //to point to top of stack always
+    private int top;
+    //array used to build the stack
+    private int[] stackArray;
+
+    //constructor
+    public Stack(int size) {
+        maxSize = size;
+        stackArray = new int[maxSize];
+        top = -1; //empty stack at the start
+    }
 }
