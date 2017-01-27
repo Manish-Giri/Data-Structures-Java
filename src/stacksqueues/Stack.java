52d25aef646b14b3e9c8f0977e7de26c60a5835a
MANISH-GIRI <manish.giri.me@gmail.com>
2017-01-26 23:34:01 -0500

    Stack methods - push, pop, peek

--- src/stacksqueues/Stack.java
+++ src/stacksqueues/Stack.java
@@ -18,4 +18,24 @@ public class Stack {
         stackArray = new int[maxSize];
         top = -1; //empty stack at the start
     }
+
+    //stack methods
+    //add item to stack
+    public void push(int item) {
+        //to add an item, first increment top, then add new item at new top
+        stackArray[++top] = item;
+    }
+
+    //pop item from top of stack - LIFO
+    public int pop() {
+        //to remove an item - first get item from cell pointed to by top, then reduce top
+        return stackArray[top--];
+    }
+
+    //peek - display item at top of stack, without removing it
+    public int peek() {
+        return stackArray[top];
+    }
+
+    
 }
