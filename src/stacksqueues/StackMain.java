e4f8a3378a9b6bfabfe2e33f3644bcd73eec69a5
MANISH-GIRI <manish.giri.me@gmail.com>
2017-01-27 21:48:00 -0500

    Stack main class - add 5 items, check item at top

--- src/stacksqueues/StackMain.java
+++ src/stacksqueues/StackMain.java
@@ -4,4 +4,18 @@ package stacksqueues;
  * Created by manishgiri on 1/27/17.
  */
 public class StackMain {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+
+        //add 5 items
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        System.out.println(stack.peek());
+
+
+    }
 }
