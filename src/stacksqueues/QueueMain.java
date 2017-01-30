0ab5e39f83a241d0d7e3401698d8c9f4629150c1
MANISH-GIRI <manish.giri.me@gmail.com>
2017-01-29 22:47:30 -0500

    Setup classes for Circular Queue

---
+++ src/stacksqueues/QueueMain.java
@@ -0,0 +1,15 @@
+package stacksqueues;
+
+/**
+ * Created by manishgiri on 1/29/17.
+ * implementation of a circular queue where the REAR wraps around the FRONT to improve efficiency
+ * In a NON CIRCULAR queue, if a few items are deleted from the top, FRONT moves up, and when REAR is
+ * at the max capacity, no more items can be added to the queue even though there is room
+ */
+
+class Queue {
+    
+
+}
+public class QueueMain {
+}
