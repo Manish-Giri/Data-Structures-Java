85811d6870aa22b0fdf8228b44dddcba14066232
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-10 23:51:43 -0500

    Setup add method, condition for no items

--- src/stacksqueues/PriorityQueueMain.java
+++ src/stacksqueues/PriorityQueueMain.java
@@ -20,6 +20,15 @@ class PriorityQueue {
         //number f items is 0 at first
         nItems = 0;
     }
+
+    public void add(int item) {
+        //if no items in queue, insert at pos 0
+        if(nItems == 0) {
+            pqArray[nItems] = item;
+            nItems++;
+        }
+        
+    }
 }
 
 
