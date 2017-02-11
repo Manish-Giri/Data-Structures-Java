9202136e802c73495777fed7b29ea2cf27d75fc1
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-10 23:59:35 -0500

    complete add method

--- src/stacksqueues/PriorityQueueMain.java
+++ src/stacksqueues/PriorityQueueMain.java
@@ -27,7 +27,26 @@ class PriorityQueue {
             pqArray[nItems] = item;
             nItems++;
         }
-        
+        int pos = 0;
+
+        //if items are present - compare new item with each item, insert at pos where new item
+        // is of higher priority (lesser number) than higher number
+        for(int i = 0; i < pqArray.length; i++) {
+            if(item < pqArray[i]) {
+                pos = i;
+                break;
+            }
+        }
+        for(int j = pos; j < pqArray.length; j++) {
+            //shift higher items up
+            pqArray[j+1] = pqArray[j];
+        }
+        //add item at pos
+        pqArray[pos] = item;
+
+        //increment nItems;
+        nItems++;
+
     }
 }
 
