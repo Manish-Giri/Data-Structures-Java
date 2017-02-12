a9d21e19f10184c06cbea323675b9b6b8c9ddeea
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-12 01:39:29 -0500

    Create Link class, setup LinkedList class

--- src/linkedlists/LinkedListFirstApp.java
+++ src/linkedlists/LinkedListFirstApp.java
@@ -5,6 +5,40 @@ package linkedlists;
  */
 
 public class LinkedListFirstApp {
+
+    private class Link {
+        int iData;
+        double dData;
+        //reference to next link in list
+        Link next;
+
+        Link(int idata, double ddata) {
+            iData = idata;
+            dData = ddata;
+            //next link is null by default
+            next = null;
+        }
+
+        private void displayLink() {
+            System.out.println("{ " + iData + " " + dData + " }");
+        }
+    }
+
+    private class LinkedList {
+        //reference to first link in the list
+        Link first;
+
+        LinkedList() {
+            first = null;
+        }
+
+        private boolean isEmpty() {
+            //if list is empty, first will still be null
+            return first == null;
+        }
+
+    }
+
     public static void main(String[] args) {
 
     }
