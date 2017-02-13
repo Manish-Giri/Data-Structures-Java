5d8707aaac059b0714237440875adb8e6c9ff4ea
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-12 19:45:34 -0500

    Setup class to implement Double Ended Linked List

---
+++ src/linkedlists/LinkedListSecondApp.java
@@ -0,0 +1,11 @@
+package linkedlists;
+
+/**
+ * Created by manishgiri on 2/12/17.
+ * implementation of a double ended list - with additions and deletions at front and end
+ * NOTE - different from a doubly linked list
+ */
+
+
+public class LinkedListSecondApp {
+}
