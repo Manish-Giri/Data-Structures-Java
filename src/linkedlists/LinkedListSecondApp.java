e5675e8cac7f3199e82fd6d96486b5e5ce2ac1ab
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-12 23:16:36 -0500

    Create Link Class, LinkedList class - displayLink() method

--- src/linkedlists/LinkedListSecondApp.java
+++ src/linkedlists/LinkedListSecondApp.java
@@ -8,4 +8,55 @@ package linkedlists;
 
 
 public class LinkedListSecondApp {
+
+    /**
+     * class LINK to represent each link in the list
+     */
+    private class Link {
+        int iData;
+
+        //reference to next link in list
+        Link next;
+
+        Link(int idata) {
+            iData = idata;
+            //next link is null by default
+            next = null;
+        }
+
+        private void displayLink() {
+            System.out.print("{ " + iData + " }");
+        }
+    }
+
+    /**
+     * class LINKEDLIST to represent the list
+     * double ended list - contain reference to FIRST and LAST link in list
+     */
+    private class LinkedList {
+        //reference to first link in list
+        private Link first;
+        //reference to last link in list
+        private Link last;
+
+        //constructor
+        LinkedList() {
+            first = null;
+            last = null;
+        }
+
+        //display items in list
+        private void displayList() {
+            System.out.println("List (first --> last) ");
+            Link current = first;
+            while(current != null) {
+                current.displayLink();
+                current = current.next;
+            }
+            System.out.println();
+        }
+
+        
+
+    }
 }
