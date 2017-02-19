5efad4d717fb7f6d452eb3c7d072fe9b882e03bc
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-18 23:43:00 -0500

    Setup class to implement Stack using Linked List

---
+++ src/linkedlists/LinkStackApp.java
@@ -0,0 +1,12 @@
+package linkedlists;
+
+/**
+ * Created by manishgiri on 2/18/17.
+ * implementation of a stack using a linked list
+ */
+
+public class LinkStackApp {
+    public static void main(String[] args) {
+
+    }
+}
