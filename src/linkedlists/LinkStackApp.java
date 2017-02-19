bbee194df657aab44862d542ece1af54e24d46a9
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-18 23:51:17 -0500

    Complete Link class

--- src/linkedlists/LinkStackApp.java
+++ src/linkedlists/LinkStackApp.java
@@ -6,6 +6,25 @@ package linkedlists;
  */
 
 public class LinkStackApp {
+
+    /**
+     * Link class
+     */
+    private class Link {
+        private int iData;
+        private Link next;
+
+        Link(int data) {
+            iData = data;
+            next = null;
+        }
+
+        private void displayLink() {
+            System.out.println("{ " + iData + " }");
+        }
+    }
+
+
     public static void main(String[] args) {
 
     }
