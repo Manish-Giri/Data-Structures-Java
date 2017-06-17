8167be28e07b5f215886ca7f2bdacbb2460b5905
MANISH-GIRI <manish.giri.me@gmail.com>
2017-06-16 23:49:50 -0400

    Create Link Class

--- src/linkedlists/queueUsingLinkedList/LinkQueueApp.java
+++ src/linkedlists/queueUsingLinkedList/LinkQueueApp.java
@@ -6,5 +6,23 @@ package linkedlists.queueUsingLinkedList;
  */
 public class LinkQueueApp {
 
+    /**
+     * CLASS Link
+     */
+    private class Link {
+        int data;
+        Link next;
+
+        // constructor
+        Link(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        // display each link
+        void displayLink() {
+            System.out.println(this.data + " ");
+        }
+    }
 
 }
